cb5dcd68c357ee434f50dbee2581be5408cea1cd
Steve Jabour <steve@jabour.me>
2019-10-24 12:15:04 -0600

    Tokenfield now proxies args down to state manager

--- addon/components/tf-state-manager.ts
+++ addon/components/tf-state-manager.ts
@@ -10,7 +10,7 @@ export interface Option {
   [key: string]: any
 }
 
-interface FetchOptions {
+export interface FetchOptions {
   (value: string): Promise<Option[]>
 }
 
--- addon/components/tokenfield.ts
+++ addon/components/tokenfield.ts
@@ -1,7 +1,22 @@
+import { Option, FetchOptions } from './tf-state-manager';
+import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-interface TokenfieldArgs {}
+interface TokenfieldArgs {
+  value?: string;
+  placeholder?: string;
+  options?: Option[] | FetchOptions;
+  onSelect: (option: Option | Option[]) => Promise<void> | void;
+}
 
 export default class Tokenfield extends Component<TokenfieldArgs> {
+  @tracked private value: string = this.args.value || '';
+  @tracked private placeholder: string = this.args.placeholder || '';
 
+  @action onSelect(option: Option | Option[]) {
+    if (this.args.onSelect) {
+      this.args.onSelect(option);
+    }
+  }
 }
